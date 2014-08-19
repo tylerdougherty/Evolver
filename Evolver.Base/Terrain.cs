d61cd9ddf5ea88eac432a2cf87af8c0958e03e2b
Tyler Dougherty <twd3@students.calvin.edu>
2014-08-18 22:20:40 -0400

    Initial commit

---
+++ Evolver.Base/Terrain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolver.Base
+{
+	class Terrain
+	{
+		enum TerrainType
+		{
+			Grass
+		};
+	}
+}
---
+++ Evolver.Base/World.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolver.Base
+{
+	class World
+	{
+		private Terrain[][] terrain;
+		private WorldObject[][] world;
+
+		public World(int xSize, int ySize)
+		{
+
+		}
+	}
+}
